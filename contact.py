9dfb002cc68044e2bb01fd567eb132c8ca45de83
DavidNganga <ngashiedavid@gmail.com>
2018-03-29 17:22:21 +0300

    ran further tests

--- contact.py
+++ contact.py
@@ -1,15 +1,21 @@
+import pyperclip
+
 class UserData:
     contact_list = []
-    '''
+    def __init__(self,name,password):
+        self.name = name
+        self.password = password
+        
+        '''
 
-     __init__ method that helps us define properties for our objects.
+        __init__ method that helps us define properties for our objects.
 
-        Args:
+         Args:
             name: New contacname.
             last_name : New contact last name.
             number: New contact phone number.
             email : New contact email address.
-    '''
+        '''
     
     def save_contact(self):
 
@@ -17,10 +23,42 @@ class UserData:
         save_contact method saves contact objects into contact_list
         '''
 
-        UserData.contact_list.append(self)  
+        UserData.contact_list.append(self) 
+    @classmethod
 
-    def __init__(self,name,password):
+    def find_by_name(cls,name):
+        '''
+        Method that takes in a name and returns a contact that matches that name.
 
-        self.name = name
-        self.password = password
+        Args:
+            name: name to search for
+        Returns :
+            Contact of person that matches the number.
+        '''
+
+        for contact in cls.contact_list:
+            if contact.name == name:
+                return contact    
+
+    @classmethod
+    def contact_exist(cls,name):
+        '''
+        Method that checks if a contact exists from the contact list.
+        Args:
+            name: Name to search if it exists
+        Returns :
+            Boolean: True or false depending if the contact exists
+        '''
+        for contact in cls.contact_list:
+            if contact.name == name:
+                    return True
+
+        return False 
+    @classmethod
+    def display_contacts(cls):
+        '''
+        method that returns the contact list
+        '''
+        return cls.contact_list 
+    
  
--- contact_test.py
+++ contact_test.py
@@ -1,5 +1,7 @@
 import unittest
 from contact import UserData
+import pyperclip
+
 class TestUserData(unittest.TestCase):
     def setUp(self):
         '''
@@ -22,15 +24,6 @@ class TestUserData(unittest.TestCase):
         self.new_contact.save_contact() # saving the new contact
         self.assertEqual(len(UserData.contact_list),1)
 
-    def test_save_multiple_contact(self):
-            '''
-            test_save_multiple_contact to check if we can save multiple contact
-            objects to our contact_list
-            '''
-            self.new_contact.save_contact()
-            test_contact = UserData("Test","user","0712345678","test@user.com") # new contact
-            test_contact.save_contact()
-            self.assertEqual(len(UserData.contact_list),2)
     def tearDown(self):
             '''
             tearDown method that does clean up after each test case has run.
@@ -46,7 +39,37 @@ class TestUserData(unittest.TestCase):
             test_contact = UserData("Test","user")# new contact
             test_contact.save_contact()
             self.assertEqual(len(UserData.contact_list),2)
-           
+    def test_find_contact_by_name(self):
+        '''
+        test to check if we can find a contact by  name and display information
+        '''
+
+        self.new_contact.save_contact()
+        test_contact = UserData("Test","user") # new contact
+        test_contact.save_contact()
+
+        found_contact = UserData.find_by_name("Test")
+
+        self.assertEqual(found_contact.password,test_contact.password)        
+    def test_contact_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the contact.
+        '''
+
+        self.new_contact.save_contact()
+        test_contact = UserData("Test","user") # new contact
+        test_contact.save_contact()
+
+        contact_exists = UserData.contact_exist("Test")
+
+        self.assertTrue(contact_exists) 
+    def test_display_all_contacts(self):
+        '''
+        method that returns a list of all contacts saved
+        '''
+
+        self.assertEqual(UserData.display_contacts(),UserData.contact_list) 
+              
 
 if __name__ == '__main__':
     unittest.main()
