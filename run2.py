de3ef5de3ee525e3dab0dcd8fcccbcb27819065f
DavidNganga <ngashiedavid@gmail.com>
2018-04-03 09:08:06 +0300

    deleted run2.py

--- run2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3.6
-import random
-
-s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-passlen = 8
-p =  "".join(random.sample(s,passlen ))
-print (p)   
